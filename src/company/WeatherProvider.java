9bbee199601f9247062297bb26d178f1ce853156
aligodzhi005 <anatov9191@gmail.com>
2022-03-05 13:59:15 +0300

    finished WEatherProvider

---
+++ src/company/WeatherProvider.java
@@ -0,0 +1,19 @@
+package company;
+
+import company.Aircrafts.Coordinates;
+
+public class WeatherProvider {
+    private static WeatherProvider weatherProvider;
+    private final String[] weatherType = {"RAIN", "FOG", "SUN", "SNOW"};
+
+    public static WeatherProvider getWeatherProvider() {
+        if (weatherProvider == null)
+            weatherProvider = new WeatherProvider();
+        return weatherProvider;
+    }
+
+    public String getCurrentWeather(Coordinates coordinates) {
+        int index = (coordinates.getHeight() + coordinates.getLatitude() + coordinates.getLongitude()) % 4;
+        return weatherType[index];
+    }
+}
